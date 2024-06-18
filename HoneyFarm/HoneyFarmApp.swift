c865e24f86ff33343c07d4d7c637f7a85be10d8a
meb <4982406+barrasso@users.noreply.github.com>
2024-06-18 14:19:52 -0400

    Initial Commit

---
+++ HoneyFarm/HoneyFarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneyFarmApp.swift
+//  HoneyFarm
+//
+//  Created by Mark Barrasso on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct HoneyFarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
